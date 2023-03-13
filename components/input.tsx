e7e50b27b1104131d1d2f278cd1f39c743d2209e
ummagohil <umgo0510@gmail.com>
2023-03-13 20:27:48 +0000

    tidy up

--- components/input.tsx
+++ components/input.tsx
@@ -8,33 +8,31 @@ const Input = (props: {
   ErrorTwo: string | undefined;
 }) => {
   return (
-    <>
-      <main className="input-section">
-        <section className="form-section">
-          <label htmlFor="actorActressOne">Actor/Actress One:</label>
-          <input
-            type="text"
-            id="actorActressOne"
-            className={props.ErrorOne}
-            onChange={props.idOne}
-          />
-          <label htmlFor="actorActressTwo">Actor/Actress Two:</label>
-          <input
-            type="text"
-            id="actorActressOne"
-            className={props.ErrorTwo}
-            onChange={props.idTwo}
-          />
-        </section>
-        <button
-          onClick={props.handleSearch}
-          type="submit"
-          className="submit-button"
-        >
-          Find Film(s)
-        </button>
-      </main>
-    </>
+    <main className="input-section">
+      <section className="form-section">
+        <label htmlFor="actorActressOne">Actor/Actress One:</label>
+        <input
+          type="text"
+          id="actorActressOne"
+          className={props.ErrorOne}
+          onChange={props.idOne}
+        />
+        <label htmlFor="actorActressTwo">Actor/Actress Two:</label>
+        <input
+          type="text"
+          id="actorActressOne"
+          className={props.ErrorTwo}
+          onChange={props.idTwo}
+        />
+      </section>
+      <button
+        onClick={props.handleSearch}
+        type="submit"
+        className="submit-button"
+      >
+        Find Film(s)
+      </button>
+    </main>
   );
 };
 export default Input;
--- components/result.tsx
+++ components/result.tsx
@@ -5,55 +5,52 @@ import { MoreInfo } from "../types/types";
 const Result = (props: { moreInfo: Array<MoreInfo> } | any) => {
   const [showIndex, setShowIndex] = useState<number>(-1);
   return (
-    <>
-      <main className="results-main">
-        {props.moreInfo?.results < 1 && <h2>No Films Found</h2>}
-        {props.moreInfo?.results?.map((film: MoreInfo, index: number) => (
-          <section key={film.id} className="results">
-            <div className="result-image">
-              <Image
-                src={`http://image.tmdb.org/t/p/w500${film.poster_path}`}
-                alt={film.title}
-                width={150}
-                height={230}
-                className="film-img"
-              />
-            </div>
-            <div className="result-info">
-              <h3>
-                {film.title} ({new Date(film.release_date).getFullYear()})
-              </h3>
-              <p className="stars-wrap">
-                Rating:{Array(Math.round(film.popularity)).fill("⭐")} (
-                {film.popularity.toFixed(2)})
-              </p>
-
-              {showIndex === index ? (
-                <p>{film.overview}</p>
-              ) : (
-                <p>{`${film.overview.substring(0, 250)}...`}</p>
-              )}
-              {film.overview.length > 250 && (
-                <button
-                  className={showIndex === index ? "hide" : "show"}
-                  onClick={() =>
-                    showIndex === index ? setShowIndex(-1) : setShowIndex(index)
-                  }
-                >
-                  <Image
-                    priority
-                    src="/chevron.svg"
-                    height={32}
-                    width={32}
-                    alt="chevron"
-                  />
-                </button>
-              )}
-            </div>
-          </section>
-        ))}
-      </main>
-    </>
+    <main className="results-main">
+      {props.moreInfo?.results < 1 && <h2>No Films Found</h2>}
+      {props.moreInfo?.results?.map((film: MoreInfo, index: number) => (
+        <section key={film.id} className="results">
+          <div className="result-image">
+            <Image
+              src={`http://image.tmdb.org/t/p/w500${film.poster_path}`}
+              alt={film.title}
+              width={150}
+              height={230}
+              className="film-img"
+            />
+          </div>
+          <div className="result-info">
+            <h3>
+              {film.title} ({new Date(film.release_date).getFullYear()})
+            </h3>
+            <p className="stars-wrap">
+              Rating:{Array(Math.round(film.popularity)).fill("⭐")} (
+              {film.popularity.toFixed(2)})
+            </p>
+            {showIndex === index ? (
+              <p>{film.overview}</p>
+            ) : (
+              <p>{`${film.overview.substring(0, 250)}...`}</p>
+            )}
+            {film.overview.length > 250 && (
+              <button
+                className={showIndex === index ? "hide" : "show"}
+                onClick={() =>
+                  showIndex === index ? setShowIndex(-1) : setShowIndex(index)
+                }
+              >
+                <Image
+                  priority
+                  src="/chevron.svg"
+                  height={32}
+                  width={32}
+                  alt="chevron"
+                />
+              </button>
+            )}
+          </div>
+        </section>
+      ))}
+    </main>
   );
 };
 
