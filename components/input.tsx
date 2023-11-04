c2ad1b158487277faf3e0af52e16080863015931
ummagohil <umgo0510@gmail.com>
2023-11-04 15:36:53 +0000

    disable submit button

--- components/input.tsx
+++ components/input.tsx
@@ -75,6 +75,7 @@ const Input = (props: {
               <button
                 onClick={props.handleSearch}
                 type="submit"
+                disabled={!values.actorActressOne || !values.actorActressTwo}
                 className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
               >
                 Find Film(s)
